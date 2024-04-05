4688996674bf55f9efbcfd7126ced192630713c8
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-04-05 16:28:07 +0530

    added top compilers

---
+++ frontend/js/constants.js
@@ -0,0 +1,31 @@
+export const top_compilers = [
+    {
+        img : 'C.svg',
+        txt : 'C'
+    },
+    {
+        img : 'c++.svg',
+        txt : 'C++'
+    },
+    {
+        img : 'java.svg',
+        txt : 'Java'
+    },
+    {
+        img : 'javascript.svg',
+        txt : 'Javascript'
+    },
+    {
+        img : 'python.svg',
+        txt : 'Python'
+    },
+    {
+        img : 'ruby.svg',
+        txt : 'Ruby'
+    },
+    {
+        img : 'rust.svg',
+        txt : 'Rust'
+    },
+]
+
