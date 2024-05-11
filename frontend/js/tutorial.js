bf8a93105db4f1b3953569ef250592992afc6824
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-05-11 15:27:06 +0530

    added prism library

---
+++ frontend/js/tutorial.js
@@ -0,0 +1,10 @@
+const contents = document.getElementById('contents')
+
+function toggle(ele){
+    if (ele.classList.contains('hide')) {
+        ele.classList.remove('hide');
+      } else {
+        ele.classList.add('hide');
+      }
+}
+
